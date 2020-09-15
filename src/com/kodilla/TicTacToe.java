8a85de64ac7be7c25d96a98231139cebd20b0762
mkrawcz <marika.krawczykowska@gmail.com>
2020-09-15 12:28:30 +0200

    Wgranie obrazow oraz wyswietlenie tla.

--- src/com/kodilla/TicTacToe.java
+++ src/com/kodilla/TicTacToe.java
@@ -1,6 +1,7 @@
 package com.kodilla;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -8,21 +9,18 @@ import javafx.stage.Stage;
 import javafx.scene.Group;
 
 
-import java.awt.*;
 import java.security.Principal;
 import java.util.Enumeration;
 
 public class TicTacToe extends Application {
 
-
-
+    private Image imageback = new Image("file:src/resources/check-in-grid.png");
 
     public static void main(String[] args) {
         launch(args);
     }
-
-    ClassLoader classLoader = getClass().getClassLoader();
-    private Image imageback = new Image(classLoader.getResource("resources/check-in-grid.png"));
+    //ClassLoader classLoader = getClass().getClassLoader();
+            //new Image(classLoader.getResource("resources/check-in-grid.png"));
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -33,10 +31,11 @@ public class TicTacToe extends Application {
         GridPane grid = new GridPane();
         grid.setBackground(background);
 
-        Scene scene = new Scene(grid, 300, 300, Color.BLACK);
+        Scene scene = new Scene(grid, 1024, 768, Color.BLACK);
 
         primaryStage.setTitle("TIC TAC TOE");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
 
     }
