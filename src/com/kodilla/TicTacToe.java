e5ec2dd333a273af82284ed52df4b20cfa0b9396
mkrawcz <marika.krawczykowska@gmail.com>
2020-09-18 21:00:27 +0200

    Wgranie obrazow oraz wyswietlenie tla.

--- src/com/kodilla/TicTacToe.java
+++ src/com/kodilla/TicTacToe.java
@@ -1,7 +1,10 @@
 package com.kodilla;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -15,6 +18,7 @@ import java.util.Enumeration;
 public class TicTacToe extends Application {
 
     private Image imageback = new Image("file:src/resources/check-in-grid.png");
+    private GridPane board = new GridPane();
 
     public static void main(String[] args) {
         launch(args);
@@ -28,10 +32,25 @@ public class TicTacToe extends Application {
         BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         Background background = new Background(backgroundImage);
 
-        GridPane grid = new GridPane();
-        grid.setBackground(background);
+        BorderPane mainBorderPane = new BorderPane();
+        mainBorderPane.setBackground(background);
 
-        Scene scene = new Scene(grid, 1024, 768, Color.BLACK);
+        board.setPadding(new Insets(50, 0, 0, 50));
+        board.setHgap(12);
+        board.setVgap(5);
+        board.setTranslateX(12);
+        board.setTranslateY(10);
+        board.setGridLinesVisible(true);
+        mainBorderPane.setCenter(board);
+
+        //dodac 9 ImageView np. z X tak aby sie pokazaly i dostosowac board do tla
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board.add(new ImageView(new Image("file:src/resources/x-sign.png")), i, j, 1, 1);
+            }
+        }
+
+        Scene scene = new Scene(mainBorderPane, 1024, 768, Color.BLACK);
 
         primaryStage.setTitle("TIC TAC TOE");
         primaryStage.setScene(scene);
